f0985e073f1df394757cf83b12ef798840758591
unknown <aj6993374@gmail.com>
2024-11-06 10:12:14 +0545

    THird week commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#python module 
+
+import math_utils
+import mathematics.trigonometry
+
+result=math_utils.add(5,3)
+
+print(result)
+
+import mathematics
+from mathematics import trigonometry
+cosine_of_45=mathematics.trigonometry.cosine(45)
+print(cosine_of_45)
---
+++ math_utils.py
@@ -0,0 +1,6 @@
+#math_utils.py
+def add(a,b):
+    return a+b
+
+def substract(a,b):
+    return a-b
